a1980e158aeb88f23324e7d1742eca12ac1a3c68
AFZAL <110601700+AFAZL@users.noreply.github.com>
2024-02-10 21:41:13 +0530

    Add files via upload

--- arrays.java
+++ arrays.java
@@ -2,20 +2,56 @@ import java.util.*;
 
 public class arrays {
     public static void main(String[] args) {
-        int array[] = new int[5];
-        array[0] = 1;
-        array[1] = 2;
-        array[2] = 3;
-        array[3] = 4;
-        array[4] = 5;
-        System.out.println(Arrays.toString(array));
-        for (int s = 0; s < 5; s++) {
-            for (int e = s; e < 5; e++) {
-                for (int z = s; z <= e; z++) {
-                    System.out.print(array[z] + " ");
-                }
-                System.out.println();
+        Scanner sc = new Scanner(System.in);
+        int A = 3, B = 3, C = 4;
+        int array[][] = new int[A][B];
+        int array2[][] = new int[B][C];
+        int res[][] = new int[A][C];
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < B; j++) {
+                System.out.println("Eneter an metrix1 element: ");
+                array[i][j] = sc.nextInt();
+
             }
         }
+        for (int i = 0; i < B; i++) {
+            for (int j = 0; j < C; j++) {
+                System.out.println("Eneter an metrix2 element: ");
+                array2[i][j] = sc.nextInt();
+
+            }
+        }
+        System.out.println("The Element of an array of m1  are: ");
+
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < B; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("The Element of an array of m1  are: ");
+        for (int i = 0; i < B; i++) {
+            for (int j = 0; j < C; j++) {
+                System.out.print(array2[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < C; j++) {
+                int ress = 0;
+                for (int b = 0; b < B; b++) {
+                    ress = ress + array[i][b] * array2[b][j];
+                    res[i][j] = ress;
+                }
+            }
+        }
+        System.out.println("The Element of an array of res is  are: ");
+        for (int i = 0; i < B; i++) {
+            for (int j = 0; j < C; j++) {
+                System.out.print(res[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
