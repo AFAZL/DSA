888893a280d6e64b897dc816ba36c107a7456e68
AFZAL <110601700+AFAZL@users.noreply.github.com>
2024-02-13 23:48:32 +0530

    Add files via upload

---
+++ code.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class code {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        System.out.println("Hello!!!");
+        // String ss = "ADDDD";
+        // String str = sc.nextLine();
+        // System.out.println(str);
+        // String res = str + " " + ss;
+        // System.out.println(res);
+        // System.out.println(str.charAt(2));
+        // System.out.println(str.length());
+        // System.out.println(str.substring(2, 7));
+        // System.out.println(str.contains("affu"));
+        // String ex = " Afzal and Tommy!";
+        // System.out.println(ex.trim());
+        // String array[] = ex.split(" , ");
+        // System.out.println(Arrays.toString(array));
+        String a = "Afzal";
+        String b = "afzal";
+        System.out.println(a.equalsIgnoreCase(b));
+        System.out.println(b.compareTo(a));
+
+    }
+}
